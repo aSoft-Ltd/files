673b54510bd3dc1c9bd99ca107da91c167808c6a
andylamax <andylamax@programmer.net>
2020-11-29 01:25:10 +0300

    Finalizing commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     id("com.android.library") version "4.1.0"
     kotlin("multiplatform") version "1.4.20"
     kotlin("plugin.serialization") version "1.4.20"
-    id("tz.co.asoft.library") version "0.0.7"
+    id("tz.co.asoft.library") version "1.0.0"
+    id("io.codearte.nexus-staging") version "0.22.00"
+    signing
 }
 
 kotlin {
@@ -16,3 +18,8 @@ kotlin {
         }
     }
 }
+
+aSoftLibrary(
+    version = vers.asoft.files,
+    description = "An exposer of files"
+)
